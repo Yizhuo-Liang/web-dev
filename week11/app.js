00b8a2d9843a46a4e98eab35e265818113e7aec6
Coulson Liang <yizhuo.liang.hello@gmail.com>
2021-11-17 11:32:25 -0500

    finished button

--- week11/app.js
+++ week11/app.js
@@ -1,10 +1,12 @@
 const prev = document.querySelector("#prevBtn");
 const next = document.querySelector("#nextBtn");
 const text = document.querySelector("#text");
+const scream = document.querySelector("#scream_image");
 let count = 1;
+let prevDisable = false;
 
 prev.addEventListener('click', () => {
-    if(count > 1) {
+    if(count > 1 && !prevDisabble) {
         count--;
         text.innerHTML = count;
     }
@@ -13,4 +15,38 @@ prev.addEventListener('click', () => {
 next.addEventListener('click', () => {
     count++;
     text.innerHTML = count;
+
+    if(count === 4) {
+        prev.style.opacity = 0;
+        prevDisable = true;
+    }
+
+    if(count === 6) {
+        text.style.color = "red";
+    }
+
+    if(count === 8) {
+        next.style.width = "5vw";
+    }
+
+    if(count === 9) {
+        next.style.width = "4vw";
+    }
+
+    if(count === 10) {
+        next.style.width = "3vw";
+    }
+
+    if(count === 11) {
+        next.style.width = "2vw";
+    }
+
+    if(count === 12) {
+        next.style.width = "1vw";
+    }
+
+    if(count===13){
+        document.body.style.backgroundImage="url('https://yizhuo-liang.github.io/pictures/scream.jpeg%27)";
+        scream.style.opacity = 1;
+    }
 });
